664f5b17ae79d03957786ad3fa0c210f06f6e4ea
GTabala <gtabala@gmail.com>
2023-03-31 17:54:38 +0100

    Added entities

---
+++ src/main/java/com/example/librarysystem/entity/Book.java
@@ -0,0 +1,60 @@
+package com.example.librarysystem.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "BOOKS")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "book_id")
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    private String isbn;
+
+    private int loans;
+
+    public Book(String title, String author, String isbn) {
+        this.author = author;
+        this.title = title;
+        this.isbn = isbn;
+        this.loans = 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getLoans() {
+        return loans;
+    }
+
+    public void setLoans(int loans) {
+        this.loans = loans;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
---
+++ src/main/java/com/example/librarysystem/entity/Loan.java
@@ -0,0 +1,55 @@
+package com.example.librarysystem.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "LOANS")
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "loan_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(name = "loan_date", nullable = false )
+    private Date loanDate;
+
+    @Column(name = "date_to_return", nullable = false )
+    private Date dateToReturn;
+
+    @Column(name = "return_date" )
+    private Date returnDate;
+
+    public Loan(User user, Date loanDate, Date dateToReturn) {
+        this.user = user;
+        this.loanDate = loanDate;
+        this.dateToReturn = dateToReturn;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+
+    public Date getLoanDate() {
+        return loanDate;
+    }
+
+
+    public Date getDateToReturn() {
+        return dateToReturn;
+    }
+
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+}
